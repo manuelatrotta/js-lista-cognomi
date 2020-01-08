a40d96b315657ce4026ca8f9096bdb49c40cbbfa
manuelatrotta <manuelat88@hotmail.it>
2020-01-08 15:18:28 +0100

    prompt surname

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 alert('hello');
+//Chiedi all’utente il cognome,
+var cognomeUtente = prompt('inserisci il tuo cognome');
+console.log(cognomeUtente);
+//inseriscilo in un array con altri cognomi,
+//stampa la lista ordinata alfabeticamente (in html ul > li).
+//Scrivi anche la posizione della lista in cui il nuovo utente si trova.
