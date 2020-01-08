31d7eb981a39e47e690817e8d1c5f21adff538f2
manuelatrotta <manuelat88@hotmail.it>
2020-01-08 15:32:20 +0100

    inserimento cognomeUtente in lista

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 alert('hello');
-//Chiedi all’utente il cognome,
+//Chiedi all’utente il cognome
 var cognomeUtente = prompt('inserisci il tuo cognome');
 console.log(cognomeUtente);
-//inseriscilo in un array con altri cognomi,
+//inseriscilo in un array con altri cognomi
+var cognomi = ['cifu', 'belmonte', 'golia', 'trotta', 'sangiovanni', 'sisio', 'maida', 'senatore'];
+cognomi.push(cognomeUtente);
+console.log(cognomi);
 //stampa la lista ordinata alfabeticamente (in html ul > li).
+
+
 //Scrivi anche la posizione della lista in cui il nuovo utente si trova.
